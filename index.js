0e9a16405370743baffcb1b99face50f6310f723
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-04-02 03:44:27 -0500

    resolve errors

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from 'express';
-import connectDB from './db/connectDB';
+import connectDB from './db/connectDB.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
