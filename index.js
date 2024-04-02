537b253f3e09020ab85dbf59820739b727a62083
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-04-02 03:22:51 -0500

    connecting Mongoose

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import connectDB from './db/connectDB';
 
 const app = express();
 const port = process.env.PORT || 8080;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+connectDB(DATABASE_URL);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
