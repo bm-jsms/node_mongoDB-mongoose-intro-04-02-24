f8fe693190b5b0696c7423aa014f297289c97a19
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-04-02 15:56:33 -0500

    add: mongoose schema - types data

--- models/Movies.js
+++ models/Movies.js
@@ -1,3 +1,19 @@
 import mongoose from 'mongoose';
 
-const moviesSchema = new mongoose.Schema({});
+const moviesSchema = new mongoose.Schema({
+	name: { type: String, required: true, trim: true },
+	ratings: { type: Number, required: true, trim: true, min: 1, max: 5 },
+	money: {
+		type: mongoose.Decimal128,
+		required: true,
+		validate: (v) => v > 10,
+	},
+	genre: { type: Array, required: true },
+	isActive: { type: Boolean, default: true },
+	comments: [
+		{
+			value: { type: String, trim: true },
+			published: { type: Date, default: Date.now },
+		},
+	],
+});
